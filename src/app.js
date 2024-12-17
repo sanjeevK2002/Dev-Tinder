9e1d475fa1c69c32bae3a359c67868e6ee6a162a
sanjeevK2002 <sanjeevpaswan2002@gmail.com>
2024-12-17 11:12:33 +0530

    express server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-console.log("sanjeev kumar paswan");
+const express=require("express");
+const app=express();
+
+
+
+app.use("/host",(req,res)=>{
+    res.send("this is use first2")
+})
+
+app.use("/test",(req,res)=>{
+    res.send("this is use first test")
+})
+
+app.use("/",(req,res)=>{
+    res.send("this is use first")
+})
+
+app.get("/",(req,res)=>{
+    res.send("Hello express");    
+});
+
+app.listen(3000,()=>{
+    console.log("server is running")
+})

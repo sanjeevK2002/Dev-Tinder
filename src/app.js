fe3240f280a914aa028fa8b40f9280d63438e48f
sanjeevK2002 <sanjeevpaswan2002@gmail.com>
2024-12-20 04:14:36 +0530

    dynamic signup data api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express=require("express");
 const connectDB = require("./config/database")
 const app=express();
+const User = require("./models/user")
+// app.use is responsible for all request (get,post....).
+//  express.json ,It handling  the request and processing that JSON data 
+//Express.json reads the json obect and convert it into javascript object and 
+// add this js object back to req in req body
+app.use(express.json()); 
+app.post("/signup",async (req,res)=>{
+
+console.log(req.body)
+    //Creating a new instance of the User model
+const user = new User(req.body); 
+try {
+    await user.save();
+    res.send("User added successfully");
+} catch (error) {
+    res.status().send("Error Saving the user : ",error.meesage)
+}   
+
+
+})
 
 connectDB()
 .then(()=>{

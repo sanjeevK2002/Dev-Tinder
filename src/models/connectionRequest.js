9c64247645ff1435e270513b2788015bf6f19577
sanjeevK2002 <sanjeevpaswan2002@gmail.com>
2024-12-26 16:13:50 +0530

    connection request

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -20,8 +20,16 @@ const connectionRequestSchema = new mongoose.Schema(
 },
 {timestamps:true}
 );
-const ConnectionRequest = new mongoose.model(
+connectionRequestSchema.pre("save" , function(next){
+    const connectionRequest=this;
+    // check if the fromUserId is same as toUserId
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to yourself")
+    }
+    next(); 
+})
+const ConnectionRequestModel = new mongoose.model(
     "ConnectionRequest",
     connectionRequestSchema
 );
-module.exports = ConnectionRequest;
+module.exports = ConnectionRequestModel;
--- src/routes/request.js
+++ src/routes/request.js
@@ -2,12 +2,36 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest")
+const User =require("../models/user")
 
 requestRouter.post("/request/send/:status/:toUserId",userAuth, async (req,res)=>{
    try{
         const fromUserId = req.user._id;
         const toUserId = req.params.toUserId;
         const status = req.params.status;
+        
+        const allowedStatus = ["ignored","interested"];
+        if(!allowedStatus.includes(status)){
+            return res
+            .status(400)
+            .json({message : "Invalid status type :" +status})
+        }
+
+
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or:[
+                {fromUserId,toUserId},
+                {fromUserId: toUserId , toUserId : fromUserId}
+            ]
+        })
+        if(existingConnectionRequest){
+            return res.status(400).send({message : `Connection Request of ${req.user.firstName} is already presence`})
+        }
+
+        const toUser = await User.findById(toUserId);
+        if(!toUser){
+            return res.status(404).send({message : `User not found`})
+        }
 
         const connectionRequest = new ConnectionRequest({
             fromUserId,
@@ -18,7 +42,7 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth, async (req,res)=>
         const data = await connectionRequest.save();
         res.json({
 
-            message : `Connection request sent successfully`,
+            message : req.user.firstName + " is "+ status + " in "+toUser.firstName,
             data,
         })
    }catch(error){

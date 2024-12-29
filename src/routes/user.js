f099a533a93aacd6dde434860c3d3fb73c260774
sanjeevK2002 <sanjeevpaswan2002@gmail.com>
2024-12-30 03:02:49 +0530

    feed for user

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const { set } = require("mongoose");
+const user = require("../models/user");
 const User_Safe_Data = "firstName lastName  photourl  age  gender  about";
 //get all the pending request for the loggedIn User
 userRouter.get("/user/request/received",userAuth , async (req,res)=>{
@@ -50,6 +52,52 @@ userRouter.get("/user/connection",userAuth , async (req,res)=>{
     } catch (error) {
         if(!res.headersSent){
             res.status(500).send("ERROR : " + error.message);
+        }
+    }
+})
+
+// feed
+userRouter.get("/feed",userAuth , async (req,res)=>{
+    try{
+        //User should see all the user card except
+        //0. his owm card
+        //1 . his connection
+        //2. ignored people
+        //3. already sent the connection request
+        const loggedInUser = req.user;
+         
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit>50?50:limit;
+        const skip = (page - 1)*limit;
+        
+
+        const connectionRequest = await ConnectionRequest.find({
+           $or:[{fromUserId : loggedInUser._id},
+            {toUserId:loggedInUser._id}
+           ],
+        }).select("fromUserId , toUserId")
+
+        const hideUserFromFeed = new Set();
+        connectionRequest.forEach((req)=>{
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        })
+
+        const users = await user.find({
+          $and :  [{_id:{$nin : Array.from(hideUserFromFeed)}},
+            {_id:{$ne:loggedInUser._id}}
+          ]
+        }).select(User_Safe_Data)
+        .skip(skip)
+        .limit(limit)
+        res.send(users)
+
+        
+
+    }catch(error){
+        if(!res.headersSent){
+            res.status(400).send({message : error.message})
         }
     }
 })
